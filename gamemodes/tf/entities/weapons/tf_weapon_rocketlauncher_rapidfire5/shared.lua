9b284b3ebb08f6a6f82a323ce670486ddf57547e
ORLAN11813 <orlanflblack@gmail.com>
2025-05-29 20:17:34 -0600

    Update shared.lua

--- gamemodes/tf/entities/weapons/tf_weapon_rocketlauncher_rapidfire5/shared.lua
+++ gamemodes/tf/entities/weapons/tf_weapon_rocketlauncher_rapidfire5/shared.lua
@@ -52,7 +52,7 @@ SWEP.Primary.ClipSize		= 9
 SWEP.Primary.DefaultClip	= SWEP.Primary.ClipSize
 SWEP.Primary.Delay          = 0.8 * 0.2
 
-SWEP.IsRapidFire = false
+SWEP.IsRapidFire = true
 SWEP.ReloadSingle = true
 
 SWEP.HoldType = "PRIMARY"
